7eecb4b837151924c015bc4510e5698bf740c868
lucadard <lucadardenne@hotmail.com>
2022-08-03 15:23:29 -0300

    deleted unused service

--- src/services/getSuggestions.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-const API_KEY = '1pEskHbVQ22S4OFAPl78nMC7QF46bU8c'
-
-export default async (term: string): Promise<Array<{ name: string }>> => {
-    const res = await fetch(`https://api.giphy.com/v1/related/gif_id=${id}?api_key=${API_KEY}`)
-    return (await res.json()).data
-}
